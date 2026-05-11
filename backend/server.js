0e2b0108c0f34bb012e867d854d4553c5d7e93c5
Isac Gomes <isacfisio@gmail.com>
2026-05-11 18:12:19 -0300

    inserindo modulos frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+app.use(cors()); // Habilita CORS para permitir requisições de diferentes origens
 require('dotenv').config();   // Carrega as variáveis de ambiente do arquivo .env
+const PORT = process.env.PORT || 3000;
 app.use(express.json()); // Middleware para parsear JSON de vir primeiro pq ele traduz o corpo da requisiçao
 app.use(express.urlencoded({ extended: true })); // Middleware para parsear dados de formulário
 const routeUser = require('./routes/user'); // Importa as rotas do usuário
+
 const mongoose = require('mongoose');
 
 mongoose.connection.on('error', (err) => console.error('Erro de conexão com o MongoDB:', err)); // Adiciona um listener para erros de conexão com o MongoDB
@@ -15,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
 
 
-app.use('/users', routeUser); // Usa as rotas do usuário com o prefixo /api
+app.use('/api/users', routeUser); // Usa as rotas do usuário com o prefixo /api
 
 app.get('/api/doctor', (req, res) => {
     res.send('API de médicos!');
