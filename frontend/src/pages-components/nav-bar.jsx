20728fc7122a7e10d6e9adfa347134658195dd4e
Isac Gomes <isacfisio@gmail.com>
2026-05-16 21:59:07 -0300

    termino do navBar

--- frontend/src/pages-components/nav-bar.jsx
+++ frontend/src/pages-components/nav-bar.jsx
@@ -2,18 +2,16 @@ import { useNavigate } from "react-router-dom";
 export function NavBar({userName}) {
     const navigate = useNavigate(); 
     return (
-        <nav className="navbar  "
-            style={{ backgroundColor: '#FFFFFF', minHeight: '30px'}}>
+        <nav className="navbar"
+            style={{ backgroundColor: '#FFFFFF', minHeight: '30px', position:'fixed', top:'0',left:'20%',width:'80%', borderBottom:'1px solid rgba(0,0,0,0.08)'}}>
             <div className="container-fluid" style={{display:"flex", justifyContent:'space-between'}}>
-                <a className="navbar-brand " onClick={()=> navigate('/dashboard')}
-                style={{color: '#e9ecef',textDecoration:'none',margin:'7px 7px' ,color:'#1E6B65' }}>
-                MED Assistente 
-                </a>
-                <div style={{fontSize:'13px',margin:'7px'}}>Bem vindo ao MED!! Dr:(a)  {userName.toUpperCase()}</div>
+                <span style={{fontSize:'18px',margin:'10px 0px 0px 10px'}}><p style={{fontSize:'14px' ,margin:'6px 0px 3px 5px'}}>Bem vindo,</p>
+                    Dr<span style={{fontSize:'5px'}}>(a).</span>  {userName.toUpperCase()}
+                    </span>
                 <button
                     className="btn"
                     onClick={() => navigate('/login')}
-                    style={{ height:'22px', width:'50px',borderRadius:'15%', margin:'7px 10px', backgroundColor: '#FFFFFF'}}>
+                    style={{ height:'22px', width:'50px',borderRadius:'15%', margin:'25px 10px 0px 0px', backgroundColor: '#FFFFFF'}}>
                       Sair
                 </button>
             </div>
